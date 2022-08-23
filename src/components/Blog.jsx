78964af4043b5969d962a3563c2ff21a4c15fcab
Emerson <emerson.day@gmail.com>
2022-08-22 21:10:45 -0400

    created blog component

---
+++ src/components/Blog.jsx
@@ -0,0 +1,12 @@
+function Blog({blog}) {
+  return (
+    <div>
+     
+       <h2>{blog.title}</h2>
+       <h3>{blog.author}</h3>
+      
+    </div>
+  )
+}
+
+export default Blog
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
+import {useFetch} from '../hooks/useFetch.jsx'
+import BlogList from '../components/BlogList.jsx'
 import './Home.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  const {data, isPending, error } = useFetch('http://localhost:3001/blogs')
+  return (
+    <div>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>loading...</p>}
+      {data && <BlogList blogs={data}/>}
+    </div>
+  );
 }
