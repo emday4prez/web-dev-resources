1d5dd9db1306473f805dced404f38abcb60d80bc
Emerson <emerson.day@gmail.com>
2022-08-23 17:18:39 -0400

    updaded tags css

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,14 +3,17 @@ import './Blog.css';
 
 function Blog({ blog }) {
   return (
-    <a
-      href={blog.url}
-      target='_blank'
-    >
+    <a href={blog.url} target='_blank'>
       <div className='blog'>
         <h2>{blog.title}</h2>
         <div style={{ margin: '2px' }}></div>
-        <h3>{blog.author}</h3>
+        <h3>by {blog.author}</h3>
+        <div style={{ margin: '4px' }}></div>
+        <div className='tags'>
+          {blog.tags.map((tag) => (
+            <p className='tag'> {tag} </p>
+          ))}
+        </div>
       </div>
     </a>
   );
